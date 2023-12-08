6df767fbe2e450d456be68674b099399db74f04b
Topi Ilmoni <topi.ilmoni@gmail.com>
2023-12-08 14:59:48 +0200

    Added Header component

--- frontend/src/RouteSwitch.jsx
+++ frontend/src/RouteSwitch.jsx
@@ -1,9 +1,11 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import CountrySelectionPage from "./components/CountrySelectionPage.jsx";
+import Header from "./components/Header.jsx";
 
 const RouteSwitch = () => {
     return (
         <BrowserRouter>
+            <Header/>
             <Routes>
                 <Route path="/" element={<CountrySelectionPage/>}/>
             </Routes>
