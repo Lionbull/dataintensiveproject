f23c47067b6f3e9e1ff1a15d4ea3205672773a32
GeorgyPk <georgy.pokazeev@tak.fi>
2023-12-15 12:44:49 +0200

    Setup project dependencies

--- database/initdb.sql
+++ database/initdb.sql
@@ -1,20 +1,24 @@
 CREATE TABLE Developer(
     dev_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     dev_name TEXT(128),
+    dev_description TEXT(512),
     number_of_employees NUMBER,
     website TEXT(64),
     founding_year NUMBER,
     revenue NUMBER,
+    img_url TEXT,
     company_type TEXT(64)
 )
 
 CREATE TABLE Publisher(
     pub_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     pub_name TEXT(128),
+    pub_description TEXT(512),
     number_of_employees NUMBER,
     website TEXT(64),
     founding_year NUMBER,
     revenue NUMBER,
+    img_url TEXT,
     company_type TEXT(64)
 )
 
@@ -31,6 +35,7 @@ CREATE TABLE Videogame(
     budget NUMBER,
     engine TEXT,
     country TEXT,
+    img_url TEXT,
     CONSTRAINT fk_publisher
         FOREIGN KEY(pub_id) 
 	        REFERENCES Publisher(pub_id)
