eff587b30d349d87528d0a05721db9b2a5eca2b8
Leevi <joona.mlin@gmail.com>
2023-12-16 12:50:04 +0200

    Added cors to backend

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -8,6 +9,7 @@ var apiRouter = require('./routes/api.js')
 
 var app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,12 +18,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', apiRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
